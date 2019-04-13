9369f2e76b3b9a933ec2fbcd17c62d2e0f09d189
Coursera Learner <nobody@nowhere.nul>
2019-04-13 16:09:51 +0000

    Finished assignemnt 7, retirement

---
+++ 07_retirement/retirement.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct _retire_info{
+  int months;
+  double contribution;
+  double rate_of_return;
+};
+typedef struct _retire_info retire_info;
+
+void retirement(int startAge, //in months
+		double initial, //initial savings in dollars
+		retire_info working, //info about working
+		retire_info retired) //info about being retired
+{
+  
+  double balance_monthly = initial;
+  int total_period = startAge + working.months + retired.months;
+  for(int i = startAge; i<total_period; i++){
+    int age_years = (i/12);
+    int age_months = i - (age_years*12);
+    printf("Age %3d month %2d you have $%.2f\n", age_years, age_months, balance_monthly);
+    if(i<(startAge+working.months)){
+      balance_monthly = balance_monthly+(balance_monthly*(working.rate_of_return/12)*0.01) + working.contribution;
+      }
+    else if((i>=(startAge+working.months))&&(i<(total_period))){
+      balance_monthly = balance_monthly+(balance_monthly*(retired.rate_of_return/12)*0.01) + retired.contribution;
+      }
+    }
+}
+
+int main(){
+  
+  retire_info work = {489,1000,4.5};
+  retire_info ret = {384,-4000,1};
+  retirement(327,21345,work,ret);
+}
